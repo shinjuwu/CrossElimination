9ccf357953166d102d3e9937028c0e73a4c47a9f
shinjuwu <shinjuwu0830@gmail.com>
2018-04-16 18:35:56 +0800

    新增相鄰點判斷算法

--- crossElimination.go
+++ crossElimination.go
@@ -10,20 +10,29 @@ type EliminationTask struct {
 	board [5][5]int
 	cards *list.List
 }
-type node struct {
-	index     int
-	cardIndex int
+
+type cardPos struct {
+	positionX int
+	positionY int
 }
 
-var seq = [][]int{
-	{5, 6, 7, 8, 9},
-	{4, 19, 20, 21, 10},
-	{3, 18, 25, 22, 11},
-	{2, 17, 24, 23, 12},
-	{1, 16, 15, 14, 13},
+var gamePattern = [][]int{
+	{21, 20, 19, 18, 17},
+	{22, 7, 6, 5, 16},
+	{23, 8, 1, 4, 15},
+	{24, 9, 2, 3, 14},
+	{25, 10, 11, 12, 13},
 }
-var board = [5][5]rune{}
-var test = []rune{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
+
+var directArrays = [4][2]int{
+	{0, 1},
+	{0, -1},
+	{-1, 0},
+	{1, 0},
+}
+var board = [5][5]int{}
+var test = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 1, 1, 1, 1}
+var matchCards = []cardPos{}
 
 //New is use for create a elimination task
 func New(initCards *list.List) *EliminationTask {
@@ -38,12 +47,18 @@ func (et *EliminationTask) transferCardsToBoard() {
 
 }
 
-func eliminate(cards *list.List) *list.List {
-	return nil
+func eliminate() map[cardPos][]cardPos {
+	res := map[cardPos][]cardPos{}
+	for k, v := range board {
+		for k1 := range v {
+			triggerPoint(k, k1)
+		}
+	}
+	return res
 }
 
 func transferCardsToBoard() {
-	for k, v := range seq {
+	for k, v := range gamePattern {
 		for k1, v1 := range v {
 			board[k][k1] = test[v1-1]
 		}
@@ -56,4 +71,39 @@ func transferCardsToBoard() {
 		}
 		fmt.Println()
 	}
+}
+
+func triggerPoint(x int, y int) {
+	transferCardsToBoard()
+	for i := 0; i < len(directArrays); i++ {
+		direct := directArrays[i]
+		nextCardPos := cardPos{
+			positionX: x + direct[0],
+			positionY: y + direct[1],
+		}
+		matchCard := board[x][y]
+
+		nextPoint(matchCard, nextCardPos, direct)
+	}
+	fmt.Println(matchCards)
+}
+
+func nextPoint(matchCard int, cardPosition cardPos, direct [2]int) {
+	if cardPosition.positionX < 0 || cardPosition.positionX == len(board) ||
+		cardPosition.positionY < 0 || cardPosition.positionY == len(board[0]) ||
+		board[cardPosition.positionX][cardPosition.positionY] != matchCard {
+		return
+	}
+
+	saveCards(cardPosition)
+	nextCard := cardPos{
+		positionX: cardPosition.positionX + direct[0],
+		positionY: cardPosition.positionY + direct[1],
+	}
+
+	nextPoint(matchCard, nextCard, direct)
+}
+
+func saveCards(card cardPos) {
+	matchCards = append(matchCards, card)
 }
--- crossElimination_test.go
+++ crossElimination_test.go
@@ -1,6 +1,8 @@
 package elimination
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_transferCardsToBoard(t *testing.T) {
 	tests := []struct {
@@ -12,6 +14,25 @@ func Test_transferCardsToBoard(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			transferCardsToBoard()
+		})
+	}
+}
+
+func Test_triggerPoint(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{"case1", args{0, 0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			triggerPoint(tt.args.x, tt.args.y)
 		})
 	}
 }
