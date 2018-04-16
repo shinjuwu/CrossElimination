045b8b8119dad4217e0b46965e547e83fe94d78b
shinjuwu <shinjuwu0830@gmail.com>
2018-04-16 15:54:31 +0800

    add Trans Cards To Board

--- crossElimination.go
+++ crossElimination.go
@@ -1,12 +1,29 @@
 package elimination
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 //EliminationTask is used to illustract a Elimination game eliminate task
 type EliminationTask struct {
 	board [5][5]int
 	cards *list.List
 }
+type node struct {
+	index     int
+	cardIndex int
+}
+
+var seq = [][]int{
+	{5, 6, 7, 8, 9},
+	{4, 19, 20, 21, 10},
+	{3, 18, 25, 22, 11},
+	{2, 17, 24, 23, 12},
+	{1, 16, 15, 14, 13},
+}
+var board = [5][5]rune{}
+var test = []rune{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
 
 //New is use for create a elimination task
 func New(initCards *list.List) *EliminationTask {
@@ -23,4 +40,20 @@ func (et *EliminationTask) transferCardsToBoard() {
 
 func eliminate(cards *list.List) *list.List {
 	return nil
+}
+
+func transferCardsToBoard() {
+	for k, v := range seq {
+		for k1, v1 := range v {
+			board[k][k1] = test[v1-1]
+		}
+	}
+
+	for k, v := range board {
+		fmt.Printf("index: %d ->Cards: ", k)
+		for _, v1 := range v {
+			fmt.Printf(" %v", v1)
+		}
+		fmt.Println()
+	}
 }
---
+++ crossElimination_test.go
@@ -0,0 +1,17 @@
+package elimination
+
+import "testing"
+
+func Test_transferCardsToBoard(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+		{"case1"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transferCardsToBoard()
+		})
+	}
+}
